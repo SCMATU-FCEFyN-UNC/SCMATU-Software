a6bc47bd1a33f1896d4f4b1d4fe01b1151f5b28b
Lucas-Pardina-UNC <lucas.pardina@mi.unc.edu.ar>
2026-02-19 22:35:00 -0300

    chore(rename-endpoint-files): Rename endpoint files to *_routes.py and clean up dead code

--- src/electron/backend/routes/__init__.py
+++ src/electron/backend/routes/__init__.py
@@ -1,13 +1,11 @@
-from .hello import hello_bp
-from .connection import serial_bp
-from .control import control_bp
-from .monitoring import monitoring_bp
-from .resonance import resonance_bp
-from .device import device_bp
-from .folders import folders_bp
+from .connection_routes import serial_bp
+from .control_routes import control_bp
+from .monitoring_routes import monitoring_bp
+from .resonance_routes import resonance_bp
+from .device_routes import device_bp
+from .folders_routes import folders_bp
 
 def register_routes(app):
-    app.register_blueprint(hello_bp)
     app.register_blueprint(serial_bp)
     app.register_blueprint(control_bp)
     app.register_blueprint(monitoring_bp)
--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,8 +1,3 @@
-//import { useState } from "react";
-/* import CommunicationPanel from "./components/CommunicationPanel/CommunicationPanel";
-import ControlPanel from "./components/ControlPanel/ControlPanel";
-import MonitoringPanel from "./components/MonitoringPanel/MonitoringPanel";
-import DeviceDataPanel from "./components/DeviceDataPanel/DeviceDataPanel"; */
 import { MainLayout } from "./components/MainLayout/MainLayout";
 import { BackendUrlProvider } from "./context/BackendUrlProvider";
 import { ConnectionStatusProvider } from "./context/ConnectionStatusProvider";
@@ -16,10 +11,6 @@ function App() {
         <ConnectionStatusProvider>
           <ResonanceStatusProvider>
             <MainLayout />
-            {/* <CommunicationPanel />
-            <ControlPanel />
-            <MonitoringPanel />
-            <DeviceDataPanel /> */}
           </ResonanceStatusProvider>
         </ConnectionStatusProvider>
       </BackendUrlProvider>
